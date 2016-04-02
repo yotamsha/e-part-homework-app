1128e181dfc4af7a448aaceb2d150685d61f83a0
yotam shalev <yotamsha@gmail.com>
2016-04-02 10:26:57 +0300

    Added policies to users and sites apis.

--- server/app/config/policies.js
+++ server/app/config/policies.js
@@ -48,4 +48,11 @@ module.exports.policies = {
 		// before letting any users feed our rabbits
 		// feed : ['isNiceToAnimals', 'hasRabbitFood']
 	// }
+  UserController : {
+    '*' : ['sessionAuth'],
+    session : true
+  },
+  SiteController : {
+    '*' : ['sessionAuth']
+  }
 };
